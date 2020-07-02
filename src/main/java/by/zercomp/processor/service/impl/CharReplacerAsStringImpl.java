4e3a50d0ff9bff3705d2a832ac0fccd8d5be7c16
LeviathanZC <leviathan.zercomp@gmail.com>
2020-07-02 18:32:08 +0300

    Finish implements AsString replacer.

--- src/main/java/by/zercomp/processor/service/impl/CharReplacerAsStringImpl.java
+++ src/main/java/by/zercomp/processor/service/impl/CharReplacerAsStringImpl.java
@@ -7,11 +7,14 @@ import java.util.StringJoiner;
 
 public class CharReplacerAsStringImpl implements CharReplacer {
 
+    private StringJoiner joiner;
+    private TextValidator validator;
+
     @Override
     public String replaceInEachWord(String src, int index, char symbol) {
         final String[] words = src.split("\\s");
-        StringJoiner joiner = new StringJoiner(" ");
-        TextValidator validator = new TextValidator();
+        joiner = new StringJoiner(" ");
+        validator = new TextValidator();
         for (int i = 0; i < words.length; i++) {
             if (validator.inRange(words[i], index)) {
                 words[i] = replaceCharAt(src, i, symbol);
@@ -21,21 +24,31 @@ public class CharReplacerAsStringImpl implements CharReplacer {
         return joiner.toString();
     }
 
+    @Override
+    public String replaceMakeCorrections(String src, String before, String after) {
+        return src.replaceAll(before, after);
+    }
+
+    @Override
+    public String replaceWordsBySubStr(String src, int length, String placeholder) {
+        final String[] words = src.split("\\s");
+        joiner = new StringJoiner(" ");
+        validator = new TextValidator();
+        for (int i = 0; i < words.length; i++) {
+            if(words[i].length() == length) {
+                joiner.add(placeholder);
+            } else {
+                joiner.add(words[i]);
+            }
+        }
+        return joiner.toString();
+    }
+
+
     private String replaceCharAt(String src, int index, char ch) {
         StringBuilder wordBuilder = new StringBuilder(src);
         wordBuilder.setCharAt(index, ch);
         return wordBuilder.toString();
     }
-
-    @Override
-    public String replaceMakeCorrections(String src, String before, String after) {
-        return null;
-    }
-
-    @Override
-    public String replaceWordsBySubStr(String src, int length, String substring) {
-        return null;
-    }
-
 
 }
