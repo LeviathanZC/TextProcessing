02aab0710ee06249718ceff82519d2020846734f
LeviathanZC <leviathan.zercomp@gmail.com>
2020-07-03 18:06:04 +0300

    Add method pickLetters.

--- src/main/java/by/zercomp/processor/service/CharRemover.java
+++ src/main/java/by/zercomp/processor/service/CharRemover.java
@@ -1,8 +1,10 @@
 package by.zercomp.processor.service;
 
+import by.zercomp.processor.exception.InvalidDataException;
+
 public interface CharRemover {
 
-    String removeNonLetterOrSpaceChars(String src);
+    String removeNonLetterOrSpaceChars(String src) throws InvalidDataException;
 
     String  removeWordsByLength(String src, int length, char startsWith);
 
--- src/main/java/by/zercomp/processor/service/impl/CharRemoverAsCharImpl.java
+++ src/main/java/by/zercomp/processor/service/impl/CharRemoverAsCharImpl.java
@@ -1,15 +1,41 @@
 package by.zercomp.processor.service.impl;
 
+import by.zercomp.processor.exception.InvalidDataException;
 import by.zercomp.processor.service.CharRemover;
+
 
 public class CharRemoverAsCharImpl implements CharRemover {
     @Override
-    public String removeNonLetterOrSpaceChars(String src) {
-        return null;
+    public String removeNonLetterOrSpaceChars(String src) throws InvalidDataException {
+        if (src == null) {
+            throw new InvalidDataException("argument must not be null.");
+        }
+        char[] common = src.toCharArray();
+    }
+
+    private char[] pickLetters(char[] src) {
+        int length = src.length;
+        char[] charset = new char[length];
+        for (int i = 0, index = 0; i < length; i++) {
+            if (Character.isLetter(src[i])) {
+                charset[index++] = src[i];
+            }
+        }
+        return trimToSize(charset, src.length - charset.length);
+    }
+
+    private char[] trimToSize(char[] src, int length) {
+        char[] array = new char[length];
+        for (int i = 0; i < length; i++) {
+            array[i] = src[i];
+        }
+        return array;
     }
 
     @Override
     public String removeWordsByLength(String src, int length, char startsWith) {
         return null;
     }
+
+
 }
