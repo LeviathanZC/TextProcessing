9a856f0338038b07752c1d8e7cf3f63540f2a3bf
LeviathanZC <leviathan.zercomp@gmail.com>
2020-07-03 17:57:34 +0300

    Create classes and impl standard stubs.

---
+++ src/main/java/by/zercomp/processor/service/impl/CharRemoverAsStringImpl.java
@@ -0,0 +1,15 @@
+package by.zercomp.processor.service.impl;
+
+import by.zercomp.processor.service.CharRemover;
+
+public class CharRemoverAsStringImpl implements CharRemover {
+    @Override
+    public String removeNonLetterOrSpaceChars(String src) {
+        return null;
+    }
+
+    @Override
+    public String removeWordsByLength(String src, int length, char startsWith) {
+        return null;
+    }
+}
